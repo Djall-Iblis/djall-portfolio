0b49d2ec6ab1e4a2bab9c167eca617e1af734b8f
Sébastien <sebastien.couchard@edu.devinci.fr>
2022-02-04 15:14:58 +0100

    correction v2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Rooter from "./Route/Rooter";
 
+import "./Sass/components/_app.scss"
 
 function App() {
   return (
-    <>
+    <main id="app">
       <Rooter />
-    </>
+    </main>
   );
 }
 
